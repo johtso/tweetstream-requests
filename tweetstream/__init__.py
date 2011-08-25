f6026c601a730a37c2bd57db84940cb345761e58
Rune Halvorsen <runefh@gmail.com>
2011-08-25 14:29:46 +0200

    Fixed relative imports

--- tweetstream/__init__.py
+++ tweetstream/__init__.py
@@ -38,5 +38,5 @@ class ConnectionError(TweetStreamError):
         return '<ConnectionError %s>' % self.reason
 
 
-from streamclasses import SampleStream, FilterStream
-from deprecated import FollowStream, TrackStream, LocationStream, TweetStream, ReconnectingTweetStream
+from .streamclasses import SampleStream, FilterStream
+from .deprecated import FollowStream, TrackStream, LocationStream, TweetStream, ReconnectingTweetStream
